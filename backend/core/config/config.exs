0d71edfb380c3c67c7af200c463932cc8a0813ba
Maxly Garcia <maxgarcia6@gmail.com>
2025-12-28 13:01:11 -0400

    Feat: laverage skogsra to handle multi-enviroment config setup. Remove boilerplate code to improve readability

--- backend/core/config/config.exs
+++ backend/core/config/config.exs
@@ -7,14 +7,9 @@
 # General application configuration
 import Config
 
-# config :core,
-#   ecto_repos: [Core.Repo],
-#   generators: [timestamp_type: :utc_datetime]
-
 # Configure db
-config :core, Core.Repo,
-  url: System.get_env("DATABASE_URL"),
-  pool_size: 10
+config :core,
+  ecto_repos: [Core.Repo]
 
 # Configure the endpoint
 config :core, CoreWeb.Endpoint,
