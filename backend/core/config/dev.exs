6e571005e798e7b2d329f5019d1753f8e230bf57
Maxly Garcia <maxgarcia6@gmail.com>
2026-01-28 08:17:40 -0400

    Chore: add esbuild config + dev watcher

--- backend/core/config/dev.exs
+++ backend/core/config/dev.exs
@@ -12,7 +12,9 @@ config :core, CoreWeb.Endpoint,
   code_reloader: true,
   debug_errors: true,
   secret_key_base: "dlnIlQnZjWhDpFyzyUkuUMA1iqgaYVScNPNcMB50VAk+J/x1Eu2pkNTGlqg7nk4q",
-  watchers: []
+  watchers: [
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+  ]
 
 # ## SSL Support
 #
